349abb70620c4dcdb2f488bcaf36f6b356b1d95b
Stephanie Tang <61029209+stang912@users.noreply.github.com>
2020-07-02 18:44:26 -0700

    updated files

--- StephWorkingFiles/static/js/logic.js
+++ StephWorkingFiles/static/js/logic.js
@@ -21,11 +21,10 @@ var link = "static/data/SF Find Neighborhoods.geojson";
 d3.json(link, function(data) {
   // Creating a geoJSON layer with the retrieved data
   L.geoJson(data, {
-    // Style each feature (in this case a neighborhood)
+    // Style each feature 
     style: function(feature) {
       return {
         color: "black",
-        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
         fillColor: "gray",
         fillOpacity: 0.5,
         weight: 1.5
@@ -35,27 +34,26 @@ d3.json(link, function(data) {
     onEachFeature: function(feature, layer) {
       // Set mouse events to change map styling
       layer.on({
-        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+        // Highlight mouse over section
         mouseover: function(event) {
           layer = event.target;
           layer.setStyle({
             fillOpacity: 0.9
           });
         },
-        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+        // revert back to original 
         mouseout: function(event) {
           layer = event.target;
           layer.setStyle({
             fillOpacity: 0.5
           });
         },
-        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+        // Enlarge section when clicked
         click: function(event) {
           myMap.fitBounds(event.target.getBounds());
         }
       });
-      // Giving each feature a pop-up with information pertinent to it
-      //layer.bindPopup("<h1>" + feature.properties.neighborhood + "</h1> <hr> <h2>" + feature.properties.borough + "</h2>");
+      
 
     }
   }).addTo(myMap);
