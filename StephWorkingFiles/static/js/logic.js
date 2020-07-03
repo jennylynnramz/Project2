9400e4d76d6e6a6b3cb6233c8d64cf73315b3db0
Stephanie Tang <61029209+stang912@users.noreply.github.com>
2020-07-02 18:34:25 -0700

    updated files

--- StephWorkingFiles/static/js/logic.js
+++ StephWorkingFiles/static/js/logic.js
@@ -17,21 +17,49 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 // Use this link to get the geojson data.
 var link = "static/data/SF Find Neighborhoods.geojson";
 
-var mapStyle = {
-  color: "black",
-  fillColor: "white",
-  fillOpacity: 0.5,
-  weight: 1.5
-};
-
 // Grabbing our GeoJSON data..
 d3.json(link, function(data) {
   // Creating a geoJSON layer with the retrieved data
   L.geoJson(data, {
-    // Passing in our style object
-    style: mapStyle
-  
+    // Style each feature (in this case a neighborhood)
+    style: function(feature) {
+      return {
+        color: "black",
+        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+        fillColor: "gray",
+        fillOpacity: 0.5,
+        weight: 1.5
+      };
+    },
+    // Called on each feature
+    onEachFeature: function(feature, layer) {
+      // Set mouse events to change map styling
+      layer.on({
+        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+        mouseover: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.9
+          });
+        },
+        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+        mouseout: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.5
+          });
+        },
+        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+        click: function(event) {
+          myMap.fitBounds(event.target.getBounds());
+        }
+      });
+      // Giving each feature a pop-up with information pertinent to it
+      //layer.bindPopup("<h1>" + feature.properties.neighborhood + "</h1> <hr> <h2>" + feature.properties.borough + "</h2>");
+
+    }
   }).addTo(myMap);
 });
+
 
 
